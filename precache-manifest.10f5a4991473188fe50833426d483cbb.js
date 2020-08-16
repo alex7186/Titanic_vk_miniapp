3405362430741da2fff5d2199991cd4534d9b8be
alex7186 <sasha2649058@mail.ru>
2020-08-16 15:58:46 +0300

    Updates

---
+++ precache-manifest.10f5a4991473188fe50833426d483cbb.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "046315ca5ee9d578d58eb9a7e4a7844d",
+    "url": "./index.html"
+  },
+  {
+    "revision": "303941c17ef154968e3e",
+    "url": "./static/css/2.86df01db.chunk.css"
+  },
+  {
+    "revision": "e03597e3f44e6546658f",
+    "url": "./static/css/main.981f7211.chunk.css"
+  },
+  {
+    "revision": "303941c17ef154968e3e",
+    "url": "./static/js/2.aa7ffb05.chunk.js"
+  },
+  {
+    "revision": "21ced859ea2b2d6b856d461ad6c2afed",
+    "url": "./static/js/2.aa7ffb05.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e03597e3f44e6546658f",
+    "url": "./static/js/main.23f792d3.chunk.js"
+  },
+  {
+    "revision": "2d8723c035c250205572",
+    "url": "./static/js/runtime-main.a74dc197.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "./static/media/persik.4e1ec840.png"
+  }
+]);
